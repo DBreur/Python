0771b543b89c4afb2ca3b37ce1d8adc9d183ba3a
Dion Breur <9027852@student.tcrmbo.nl>
2026-06-02 11:19:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print("tst")
+import requests
+import json
+import mysql.connector as mysql
+
+connection = mysql.connect(
+    host="localhost",
+    username="root",
+    password="",
+    databast="escaperoom"
+)
+
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM questions")
+
+data = cursor.fetchall()
+
+cursor.close()
+connection.close()
+print(data)
